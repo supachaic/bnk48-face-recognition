f6212dec0a8c52323f8478ff3abcb3a201dfba70
Supachai Chanarittichai <supachaic@outlook.com>
2018-12-15 11:55:15 +0900

    edit router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
-import { Route, Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import FaceRecognition from './components/faceRecognition';
 import CameraFaceDetect from './components/cameraFaceDetect';
 import Header from './components/Header';
-import { createBrowserHistory } from 'history';
+//import { createBrowserHistory } from 'history';
 import './App.css';
 
-var hist = createBrowserHistory();
+//var hist = createBrowserHistory();
 
 class App extends Component {
+  componentWillMount() {
+    console.log(process.env.PUBLIC_URL);
+  }
   render() {
     return (
       <div className="App">
-        <Router history={hist}>
+        <Router basename={process.env.PUBLIC_URL}>
           <div className="route">
             <Header />
-            <Route exact path="/" component={FaceRecognition} />
-            <Route exact path="/photo" component={FaceRecognition} />
+            {/* <Route exact path="/" component={FaceRecognition} /> */}
+            <Route
+              exact
+              path="/"
+              render={routerProps => (
+                <FaceRecognition routerProps={routerProps} />
+              )}
+            />
+            <Route
+              exact
+              path="/photo"
+              render={routerProps => (
+                <FaceRecognition routerProps={routerProps} />
+              )}
+            />
+            {/* <Route exact path="/photo" component={FaceRecognition} /> */}
             <Route exact path="/camera-front" component={CameraFaceDetect} />
             <Route exact path="/camera-back" component={CameraFaceDetect} />
           </div>
