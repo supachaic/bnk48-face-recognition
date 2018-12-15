64748cc512cd071aaf0773ccfda719a23b554019
Supachai Chanarittichai <supachaic@outlook.com>
2018-12-15 12:38:21 +0900

    Updates

--- precache-manifest.bf11af0a18c4b8b8ffdf5df63c920183.js
+++ precache-manifest.bf11af0a18c4b8b8ffdf5df63c920183.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/bnk48-face-recognition/static/js/runtime~main.4942cd80.js"
   },
   {
-    "revision": "d7fd62e8b0d0127ea02f",
-    "url": "/bnk48-face-recognition/static/js/main.d7fd62e8.chunk.js"
+    "revision": "8fd4b5d93657b883586b",
+    "url": "/bnk48-face-recognition/static/js/main.8fd4b5d9.chunk.js"
   },
   {
     "revision": "d2bf42edb4d329d6e7aa",
     "url": "/bnk48-face-recognition/static/js/1.d2bf42ed.chunk.js"
   },
   {
-    "revision": "d7fd62e8b0d0127ea02f",
+    "revision": "8fd4b5d93657b883586b",
     "url": "/bnk48-face-recognition/static/css/main.c8df5cd1.chunk.css"
   },
   {
-    "revision": "67b62139613c47a8d0b3891bf695e10e",
+    "revision": "a47a86743a438f8763e462e2171cfb21",
     "url": "/bnk48-face-recognition/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bnk48-face-recognition/precache-manifest.f19949b5cddc3b8d8b91e88bef3732c2.js"
+  "/bnk48-face-recognition/precache-manifest.bf11af0a18c4b8b8ffdf5df63c920183.js"
 );
 
 workbox.clientsClaim();
